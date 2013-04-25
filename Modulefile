c4f77bc56f3a6f05ba7ee098cd793ac97e7b2636
Yogesh Panchal <yogesh.panchal@fafadiatech.com>
2013-04-25 14:11:31 +0530

    added project files

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'Yogesh-django-deploy'
+version ''
+source ''
+author 'Yogesh'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: django-deploy
+#
+# This module manages django-deploy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class django-deploy {
+
+
+}
